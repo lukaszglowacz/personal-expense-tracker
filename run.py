cf3d62f516693009f2719cc1e5cc2a51e1ead652
Lukasz <bakatjur@gmail.com>
2023-03-29 12:12:24 +0000

    Add add-expense() whick add next expense and merge it with Google Sheets document

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Define the credentials
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -10,10 +12,26 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('personal-expense-tracker')
 
+# Authorize access to Google Sheets document
+SHEET = GSPREAD_CLIENT.open('personal-expense-tracker')
 expenses = SHEET.worksheet('expenses')
 
-data = expenses.get_all_values()
 
-print(data)
+def add_expense():
+    # Get expense details
+    amount = input('Enter expense amount: ')
+    category = input('Enter expense category (bills, food, pleasures: ')
+    date = input('Enter expense date (YYYY-MM-DD): ')
+
+    # Check if date is valid
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        print("Incorrect date format, shoul be YYYY-MM-DD")
+        return
+
+    # Write expense to Google Sheets document
+    row = [int(amount), category, date]
+    expenses.append_row(row)
+    print('Expense added succesfully')
