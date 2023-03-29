ce18f0193e8b9f2fefe7a29c0403195f04f66c78
Lukasz <bakatjur@gmail.com>
2023-03-29 13:32:59 +0000

    Add edit expense fn and merge edited expense with GS document

--- run.py
+++ run.py
@@ -37,9 +37,6 @@ def add_expense():
     print('Expense added succesfully')
 
 
-add_expense()
-
-
 def view_expenses():
     # Read current month expenses from Google Sheets document
     # Check if expense is for current month
@@ -51,4 +48,38 @@ def view_expenses():
             print(f"{row[2]:<12}${row[0]:<10}{row[1]:<15}")
 
 
-view_expenses()
+def  edit_expense():
+    # Read expense from Google Sheets document
+    all_rows = expenses.get_all_values()[-10:][1:]
+
+    # Display all expenses with their indexes
+    print(f'{"Index":<6}{"Date":<12}{"Amount":<10}{"Category":<15}')
+    for i, row in enumerate(all_rows):
+        print(f"{i+1:<6}{row[2]:<12}${row[0]:<10}{row[1]:<15}")
+
+    # Prompt user for index of the expense to edit
+    index = int(input('Enter the index of the expense to edit: ')) -1
+
+    # Check if index is valid
+    if index < 0 or index >= len(all_rows):
+        print('Invalid index')
+        return
+    
+    # Prompt user for new expense details
+    amount = input('Enter new expense amount: ')
+    category = input('Enter new expense category (bills, food, pleasures): ')
+    date = input('Enter new expense date (YYYY-MM-DD): ')
+
+    # Check if date is valid
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        print('Incorrect date format, should be YYYY-MM-DD')
+        return
+
+    # Update row in Google Sheets document
+    row = [int(amount), category, date]
+    expenses.update_cell(index + 2, 1, amount)
+    expenses.update_cell(index + 2, 2, category)
+    expenses.update_cell(index + 2, 3, date)
+    print('Expense updated succesfully')
